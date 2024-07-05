310caa1637fa417d611d70f6cf6ed3a43a4fc265
pakistan <babaralie@gmail.com>
2024-07-05 00:50:37 -0700

    first commit was basic this is underworking

--- src/TesterSolarClass.java
+++ src/TesterSolarClass.java
@@ -3,6 +3,10 @@ public class TesterSolarClass {
     public static void main(String[] args) {
 
     SolarSytm milkyway = new SolarSytm();
+    milkyway.moon="3 lakh/km";
+    milkyway.planet="3 crore/km";
+    milkyway.sun="faraway";
+
 
 
 
